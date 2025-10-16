4ff893bab52dc5dbabd789079bf3e4270f76f560
Emilse Arias <emilsearias@gmail.com>
2025-10-16 10:31:27 -0300

    agregamos una opcion de eliminar tarea

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,31 +1,25 @@
-const btnAgregar = document.getElementById("agregar");
-const listadoTareas = document.getElementById('listadotareas')
-btnAgregar.addEventListener("click", agregarTarea);
+const formulario = document.querySelector("form");
+const listadoTareas = document.getElementById("listadotareas");
+formulario.addEventListener("submit", agregarTarea);
 
-console.log(btnAgregar);
+listadoTareas.addEventListener('click', (e)=>{
+  console.log(e.target)
+  if(e.target.classList.contains('borrar')){
+    console.log(e.target.parentElement)
+   e.target.parentElement.remove()
+  }
+})
 
 function agregarTarea(e) {
   e.preventDefault();
-
   let input = document.querySelector("input");
   let dato = input.value;
   if (dato) {
     let tarea = document.createElement("li");
     tarea.innerHTML = `${dato} <button type="button" class="btn btn-danger borrar">Eliminar Tarea</button>`;
     listadoTareas.appendChild(tarea);
-    borrarTarea();
     input.value = "";
   } else {
     alert("Usted no ingreso una tarea");
   }
-}
-
-function borrarTarea() {
-  const tareas = document.querySelectorAll(".borrar");
-  console.log(tareas)
-  tareas.forEach((borrar) =>
-    borrar.addEventListener("click", (tarea) =>
-      tarea.currentTarget.parentElement.remove()
-    )
-  );
 }
