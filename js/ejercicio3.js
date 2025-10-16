e37b182a629fec35f3f17f824ce47df825b76a46
Emilse Arias <emilsearias@gmail.com>
2025-10-16 11:20:29 -0300

    agregamos la funcion para cargar tareas y actualizamos el localstorage al eliminar una tarea

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -2,13 +2,16 @@ const formulario = document.querySelector("form");
 const listadoTareas = document.getElementById("listadotareas");
 formulario.addEventListener("submit", agregarTarea);
 
-listadoTareas.addEventListener('click', (e)=>{
-  console.log(e.target)
-  if(e.target.classList.contains('borrar')){
-    console.log(e.target.parentElement)
-   e.target.parentElement.remove()
+listadoTareas.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.classList.contains("borrar")) {
+    console.log(e.target.parentElement);
+    e.target.parentElement.remove();
+    guardarTareas();
   }
-})
+});
+//cuando cargue el sitio web
+document.addEventListener("DOMContentLoaded", cargarTareas);
 
 function agregarTarea(e) {
   e.preventDefault();
@@ -19,21 +22,32 @@ function agregarTarea(e) {
     tarea.innerHTML = `${dato} <button type="button" class="btn btn-danger borrar">Eliminar Tarea</button>`;
     listadoTareas.appendChild(tarea);
     input.value = "";
-    guardarTareas()
+    guardarTareas();
   } else {
     alert("Usted no ingreso una tarea");
   }
 }
 
-function guardarTareas(){
-  const elementos = listadoTareas.querySelectorAll('li')
-  const arrayTareas = []
-  console.log(elementos)
-  elementos.forEach((li)=>{
-    const textoTarea = li.firstChild.textContent.trim()
-    arrayTareas.push(textoTarea)
-  })
-//guardar el arrayTareas en localstorage
-localStorage.setItem('tareasKey', JSON.stringify(arrayTareas))
+function guardarTareas() {
+  const elementos = listadoTareas.querySelectorAll("li");
+  const arrayTareas = [];
+  console.log(elementos);
+  elementos.forEach((li) => {
+    const textoTarea = li.firstChild.textContent.trim();
+    arrayTareas.push(textoTarea);
+  });
+  //guardar el arrayTareas en localstorage
+  localStorage.setItem("tareasKey", JSON.stringify(arrayTareas));
+}
 
+function cargarTareas() {
+  console.log("prueba de DOMContentLoaded");
+  //leer datos del localstorage
+  const arrayTareas = JSON.parse(localStorage.getItem("tareasKey")) || []
+
+  arrayTareas.forEach((tareaLocalstorage)=>{
+  let tarea = document.createElement("li");
+    tarea.innerHTML = `${tareaLocalstorage} <button type="button" class="btn btn-danger borrar">Eliminar Tarea</button>`;
+    listadoTareas.appendChild(tarea);
+  })
 }
